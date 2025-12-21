6eed72ebc60847277717952c891493c2ff8c7175
semanavasco <semanavasco@gmail.com>
2025-12-21 19:40:18 +0100

    Fearless concurrency chapter

---
+++ mutexes/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
+#[allow(dead_code)]
+fn first_example() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {m:?}");
+}
---
+++ threads/src/main.rs
@@ -0,0 +1,25 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..=10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..=5 {
+        println!("hi number {i} from main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+}
