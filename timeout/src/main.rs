171b6e7af1d1d0b18c56af96bcc97d626e0f5e95
semanavasco <semanavasco@gmail.com>
2025-12-26 20:21:16 +0100

    Async abstraction of timeout function

---
+++ timeout/src/main.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+
+use trpl::Either;
+
+fn main() {
+    trpl::run(async {
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "Finally finished"
+        };
+
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(msg) => println!("Succeeded with '{msg}'"),
+            Err(d) => println!("Failed after {} seconds", d.as_secs()),
+        }
+    });
+}
+
+async fn timeout<F: Future>(future_to_try: F, max_time: Duration) -> Result<F::Output, Duration> {
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
