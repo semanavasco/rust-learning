e44b7bade45e91939be0ec8da3fdf4028289c363
semanavasco <semanavasco@gmail.com>
2025-12-27 12:46:41 +0100

    Reformatted file

--- streams/src/main.rs
+++ streams/src/main.rs
@@ -2,12 +2,17 @@ use trpl::StreamExt;
 
 fn main() {
     trpl::run(async {
-        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let iter = values.iter().map(|n| n * 2);
-        let mut stream = trpl::stream_from_iter(iter);
-
-        while let Some(value) = stream.next().await {
-            println!("The value was: {value}");
-        }
+        println!("=== Example 1 ===");
+        example1().await;
     });
+}
+
+async fn example1() {
+    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let iter = values.iter().map(|n| n * 2);
+    let mut stream = trpl::stream_from_iter(iter);
+
+    while let Some(value) = stream.next().await {
+        println!("The value was: {value}");
+    }
 }
